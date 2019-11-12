26c6b4868c7759a7404d8541ef333013458d09ad
LamNguyen98 <lamnguyen3298@gmail.com>
2019-11-12 18:56:02 +0700

    game update

--- Game/Game.js
+++ Game/Game.js
@@ -1,35 +1,12 @@
-function allowDrop(event) {
-
-    event.preventDefault();
-}
-function drag(event) {
-    // console.log(event)
-    event.dataTransfer.setData("text", event.target.id);
-}
-function drop(event) {
-    // console.log("drop",event)
-    event.preventDefault();
-    let data = event.dataTransfer.getData("text");
-    event.target.appendChild(document.getElementById(data))
-    // console.log(this)
-}
-
 let unit = {
     atk: 5,
     move: 2,
     atkRange: 2,
     x: 0,
     y: 0,
-    isAttack: "no"
+    isAttack: "no",
+    img: "img1"
 }
-
-$(".gamecell").on("drop", function (event) {
-    event.preventDefault();
-    event.stopPropagation();
-    unit.x = this.id.split("_")[0];
-    unit.y = this.id.split("_")[1];
-})
-
 let unit1 = {
     relation: "ally"
 }
@@ -42,21 +19,34 @@ let unit3 = {
     hp: 8
 }
 
-let upperBoard = ["", "", "", unit2, unit3, unit1, "", "", "", ""]
-let lowerBoard = ["", "", "", "", "", "", "", "", "", ""]
+let Player1 = { relation: "player", hp: 20 }
+let Player2 = { relation: "player", hp: 20 }
+
+let upperBoard = [Player1, "", "", "", "", "", "", "", "", "", "", Player2]
+let lowerBoard = [Player1, "", "", "", "", "", "", "", "", "", "", Player2]
+
+function assign(event) {
+    console.log(event)
+}
+
+
+// $(".gamecell").on("drop", function (event) {
+//     event.preventDefault();
+//     event.stopPropagation();
+//     unit.x = this.id.split("_")[0];
+//     unit.y = this.id.split("_")[1];
+// })
 
 function setUp() {
-    console.log(unit.x, unit.y)
     let current = unit.x.valueOf()
-
     if (unit.y == 1) {
         upperBoard[current] = unit
-        console.log(upperBoard)
     }
     else if (unit.y == 2) {
         lowerBoard[current] = unit
-        console.log(lowerBoard)
     }
+    console.log(upperBoard)
+    console.log(lowerBoard)
 }
 
 function movement(board, unit) {
@@ -66,13 +56,14 @@ function movement(board, unit) {
     console.log(maxLocation)
     for (i = 1; i <= unit.move; i++) {
         let location1 = Number(currentLocation) + Number(i)
-        if (board[location1].relation == "ally") { }
-        else if (board[location1].relation == "enemy") {
+        if (board[location1].relation === "ally") { }
+        else if (board[location1].relation === "enemy") {
             attack(board, unit)
-            if (board[location1] == "" && location1 <= maxLocation) {
+            if (board[location1] === "" && location1 <= maxLocation) {
                 finalLocation = location1
             }
         }
+        else if (board[location1].relation === "player") { break }
         else { finalLocation = location1 }
     }
     board[currentLocation] = ""
@@ -80,17 +71,24 @@ function movement(board, unit) {
     attack(board, unit)
 }
 
-
 function attack(board, unit) {
-    if (unit.isAttack == "no") {
+    if (unit.isAttack === "no") {
         let currentLocation = board.indexOf(unit)
         for (j = 1; j <= unit.atkRange; j++) {
             let enemyLocation = Number(currentLocation) + Number(j)
             console.log(enemyLocation)
-            if (board[enemyLocation].relation == "enemy") {
+            if (board[enemyLocation].relation === "enemy") {
                 board[enemyLocation].hp -= unit.atk
                 if (board[enemyLocation].hp < 1) {
                     board[enemyLocation] = "";
+                }
+                unit.isAttack = "yes"
+                break
+            }
+            else if (board[enemyLocation].relation === "player") {
+                board[enemyLocation].hp -= unit.atk
+                if (board[enemyLocation].hp < 1) {
+                    gameWon()
                 }
                 unit.isAttack = "yes"
                 break
@@ -99,10 +97,17 @@ function attack(board, unit) {
     }
     else { }
 }
+
 function endturn() {
-    movement(upperBoard, unit);
-    update()
+    if (unit.y == 1) {
+        movement(upperBoard, unit)
+    }
+    else if (unit.y == 2) {
+        movement(lowerBoard, unit)
+    }
+    // update()
     console.log(upperBoard)
+    console.log(lowerBoard)
 }
 
 function update() {
@@ -113,5 +118,8 @@ function update() {
 }
 function reset() {
     unit.isAttack = "no"
+}
+function gameWon() {
+    alert("You win")
 }
 
